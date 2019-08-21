7b76e328fc8fe98a1b96da495c7bc6ede99fa476
sachinkum123 <48723518+sachinkum123@users.noreply.github.com>
2019-08-21 12:26:18 +0530

    Update example.rb file

--- example.rb
+++ example.rb
@@ -1,3 +1,4 @@
+puts 'INDIA: '
 puts 'India is a country in South Asia. '
 puts 'It is the seventh-largest country by area, '
 puts 'the second-most populous country, and the most populous democracy in the world.'
